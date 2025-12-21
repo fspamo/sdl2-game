9919c06ac3910d0f2197114d96684b875a230afc
fspamo <113922120+fspamo@users.noreply.github.com>
2025-12-21 18:07:42 +0300

    cleaned up the code a bit

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
-#include <SDL2/SDL.h>
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-void player_movement(SDL_Rect box)
+void player_movement(int *running, SDL_Event *event, 
+		SDL_Renderer *renderer, SDL_Rect *box)
 {
+	while (*running)
+	{
+		while (SDL_PollEvent(event))
+		{
+			if (event->type == SDL_KEYDOWN)
+			{
+				if (event->key.keysym.sym == SDLK_d)
+					box->x += 20;
+				else if (event->key.keysym.sym == SDLK_a)
+					box->x -= 20;
+				else if (event->key.keysym.sym == SDLK_w)
+					box->y -= 20;
+				else if (event->key.keysym.sym == SDLK_s)
+					box->y += 20;
+				else if (event->key.keysym.sym == SDLK_0)
+					*running = 0;
+			}
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+		SDL_RenderFillRect(renderer, box);
+
+		SDL_RenderPresent(renderer);
+		SDL_Delay(16);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -48,66 +77,18 @@ int main(int argc, char *argv[])
 	box.w = 200;
 	box.h = 150;
 
-	// Set draw color to red
+	// color thing
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderFillRect(renderer, &box);
 
-	// Show what we drew
+	// idk bruh
 	SDL_RenderPresent(renderer);
 
-	// SDL_Delay(1000); // Keep window open for 3 seconds
-	while (running)
-	{
-		// Process all events
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_KEYDOWN)
-			{
-				// simple movement for the character.
-				if(event.key.keysym.sym == SDLK_d)
-				{
-					box.x = box.x + 20;
-				}
-				else if(event.key.keysym.sym == SDLK_a)
-				{
-					box.x = box.x - 20;
-				}
-				else if(event.key.keysym.sym == SDLK_w)
-				{
-					box.y = box.y - 20;
-				}
-				else if(event.key.keysym.sym == SDLK_s)
-				{
-					box.y = box.y + 20;
-				}
-				else if (event.key.keysym.sym == SDLK_0) // Press "0"
-				{
-					running = 0;
-				}
-			}
-		}
-
-		// Clear screen (black)
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-
-		// Draw red box
-		SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-		SDL_RenderFillRect(renderer, &box);
-
-		// Present the frame
-		SDL_RenderPresent(renderer);
-
-		// 16ms delay (locking it to 60 FPS to avoid 100% CPU usage)
-		SDL_Delay(16);
-	}
+	player_movement(&running, &event, renderer, &box);
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
-	// SDL_DestroyWindow(window);
-	// SDL_Quit();
 
 	return 0;
 }
